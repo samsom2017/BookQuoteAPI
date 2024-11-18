b439be956d0ad9d4cd606aa9c490704a7a51693f
samsom <samsomdawit0@gmail.com>
2024-11-18 14:24:01 +0100

    change to bookcontroller

--- BookQuoteAPI/Controllers/BooksController.cs
+++ BookQuoteAPI/Controllers/BooksController.cs
@@ -2,6 +2,161 @@
 using BookQuoteAPI.Data;
 using BookQuoteAPI.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace BookQuoteAPI.Controllers
+{
+    // launchSetting.json can be  found http://localhost:5134/swagger/index.html
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BooksController : ControllerBase
+    {
+        //inject DB
+        private readonly ApplicationDbContext _context;
+
+
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var books = await _context.Books
+                .Where(x => x.UserId == new Guid(userId))
+                .ToListAsync();
+            return books;
+        }
+
+        // GET: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var book = await _context.Books
+                .Where(x => x.UserId == new Guid(userId))
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (book == null)
+            {
+                return NotFound("Book is not found ");
+            }
+
+            return Ok(book);
+        }
+
+        // PUT: api/Books/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var existingBook = await _context.Books
+                .Where(x => x.UserId == new Guid(userId))
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingBook == null)
+            {
+                return NotFound("Book is not found ");
+            }
+
+
+            existingBook.Author = book.Author;
+            existingBook.Title = book.Title;
+            existingBook.PublishedDate = book.PublishedDate;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+            //    if (id != book.Id)
+            //    {
+            //        return BadRequest();
+            //    }
+
+            //    _context.Entry(book).State = EntityState.Modified;
+
+            //    try
+            //    {
+            //        await _context.SaveChangesAsync();
+            //    }
+            //    catch (DbUpdateConcurrencyException)
+            //    {
+            //        if (!BookExists(id))
+            //        {
+            //            return NotFound();
+            //        }
+            //        else
+            //        {
+            //            throw;
+            //        }
+            //    }
+
+            //    return NoContent();
+        }
+
+
+
+        // POST: api/Books
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<List<Book>>> PostBook(Book book)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var newBook = new Book
+            {
+                UserId = new Guid(userId),
+                Id = book.Id,
+                Title = book.Title,
+                PublishedDate = book.PublishedDate,
+                Author = book.Author,
+            };
+            _context.Books.Add(newBook);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Books.ToListAsync());
+        }
+
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var existingBook = await _context.Books
+                            .Where(x => x.UserId == new Guid(userId))
+                            .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingBook == null)
+            {
+                return NotFound("Book is not found ");
+            }
+
+            _context.Books.Remove(existingBook);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //private bool BookExists(int id)
+        //{
+        //    return _context.Books.Any(e => e.Id == id);
+        //}
+    }
+}
+
+
+/*
+using BookQuoteAPI.Data;
+using BookQuoteAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +183,7 @@ namespace BookQuoteAPI.Controllers
 
         // GET: api/Books
         [HttpGet]
+        
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -94,11 +250,11 @@ namespace BookQuoteAPI.Controllers
             return Ok(book);
         }
         */
+/*
 
-
-        // PUT: api/Books/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
+// PUT: api/Books/5
+// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+[HttpPut("{id}")]
         public async Task<IActionResult> PutBook(int id, Book book)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -169,8 +325,9 @@ namespace BookQuoteAPI.Controllers
 
           return Ok(await _context.Books.ToListAsync());
       }
+ 
         */
-
+/*
         [HttpPost]
         public async Task<ActionResult<Book>> PostBook(Book book)
         {
@@ -221,3 +378,6 @@ namespace BookQuoteAPI.Controllers
         //}
     }
 }
+
+
+*/
