47f053c988296569d851cb61f372145f9990205c
samsom <samsomdawit0@gmail.com>
2024-11-18 17:35:04 +0100

    changed WithOrigins and json

--- BookQuoteAPI/Program.cs
+++ BookQuoteAPI/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp", builder =>
     {
-        builder.WithOrigins("http://localhost:4200", "http://localhost:60908") // Angular frontend URL
+        builder.WithOrigins("https://bookquoteapp.onrender.com") // Angular frontend URL
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
