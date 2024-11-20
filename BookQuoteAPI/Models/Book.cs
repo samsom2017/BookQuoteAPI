a2628df22f3b28cf6b8228fc05aa638f89e2971b
samsom <samsomdawit0@gmail.com>
2024-11-20 18:47:36 +0100

    Add code from developer

--- BookQuoteAPI/Models/Book.cs
+++ BookQuoteAPI/Models/Book.cs
@@ -5,6 +5,7 @@ namespace BookQuoteAPI.Models
     public class Book
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
         public string Author { get; set; }
         public DateTime PublishedDate { get; set; }
